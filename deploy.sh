9d651153805e4dbfd4dd217b9ab078e3ac9f83db
brian.verbanck <brian.verbanck@student.howest.be>
2021-04-23 18:05:34 +0200

    updated the deploy files

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,6 @@ git commit -m 'deploy'
 
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:brianverb/EPorfolio.git master:gh-pages
+git push -f git@github.com:brianverb/EPorfolio.git main:gh-pages
 
 cd -
