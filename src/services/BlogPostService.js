9a59f7ee96f8b56ddc9d50a622362ed51c101563
brian.verbanck <brian.verbanck@student.howest.be>
2021-04-23 15:37:55 +0200

    corrected database url to online json-server

--- src/services/BlogPostService.js
+++ src/services/BlogPostService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `https://my-json-server.typicode.com/brianverb/eportfolio-fake-json-server`,
   withCredentials: false, 
   headers: {
     Accept: 'application/json',
